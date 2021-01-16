4962e05f2c88cfef8e734bb5eddce7ef111d31d7
Tom Watson <tom.watson@kinesis.org>
2021-01-17 10:03:38 +1100

    make it into a dataframe

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,9 @@ use kml::{
     reader::KmlReader,
     types::{Kml, Placemark}
 };
-use chrono::{DateTime, FixedOffset};
+use chrono::{DateTime, FixedOffset, Local};
+use itertools::Itertools;
+use polars::prelude::*;
 
 fn main() {
     let path = env::args().skip(1).next().expect("Didn't get a path");
@@ -18,19 +20,87 @@ fn main() {
         let mut x = unpack(kml);
         activities.append(&mut x);
     }
-    println!("{:?}", activities);
+    activities.sort_by_key(|a| a.start);
+    let df = to_dataframe(activities);
+    println!("{:?}", df);
+//    let x: Vec<_> = activities.into_iter()
+//        .scan(0.0, |state, a| {
+//            let t = match a {
+//                Activity::Driving(v) => {
+//                    *state = *state + v.distance;
+//                    v.time
+//                },
+//                Activity::Cycling(v) => {
+//                    *state = *state - v.distance;
+//                    v.time
+//                }
+//            };
+//            Some((t, state.clone()))
+//        })
+//        .collect();
+//    let grp = x.into_iter()
+//        .group_by(|(t, v)| t.with_timezone(&Local).date())
+//        .into_iter()
+//        .map(|(k, arr)| (k, arr.last().unwrap().1))
+//        .collect::<Vec<_>>();
+//    println!("{:?}", grp);
+}
+
+fn to_dataframe(recs: Vec<ActivityRecord>) -> DataFrame {
+    let distance_col = Series::new("distance", 
+                                   &recs.iter()
+                                        .map(|a| a.distance.clone())
+                                        .collect::<Vec<f64>>());
+    let activity_col = Series::new("activity",
+                                   &recs.iter()
+                                        .map(|a| match a.activity {
+                                            ActivityKind::Driving => 0,
+                                            ActivityKind::Cycling => 1 
+                                        })
+                                        .collect::<Vec<_>>());
+    let start_arr = Date64Chunked::new_from_naive_datetime("start",
+                                                           &recs.iter()
+                                                                .map(|a| a.start.with_timezone(&Local).naive_local())
+                                                                .collect::<Vec<_>>());
+    let end_arr = Date64Chunked::new_from_naive_datetime("end",
+                                                         &recs.iter()
+                                                              .map(|a| a.end.with_timezone(&Local).naive_local())
+                                                              .collect::<Vec<_>>());
+    let debt_col = Series::new("debt",
+                               &recs.iter()
+                                    .scan(0.0, |state, a| {
+                                        match a.activity {
+                                            ActivityKind::Driving => { *state = *state + a.distance; () },
+                                            ActivityKind::Cycling => { *state = *state - a.distance; () },
+                                        };
+                                        Some(state.clone())
+                                    })
+                                    .collect::<Vec<_>>());
+    DataFrame::new(vec![start_arr.into(),
+                        end_arr.into(),
+                        distance_col,
+                        activity_col,
+                        debt_col,
+    ]).expect("Couldn't build dataframe")
+}
+
+#[derive(Debug, Clone)]
+enum ActivityKind {
+    Driving,
+    Cycling
 }
 
 #[derive(Debug)]
-enum Activity {
-    Driving(ActivityValue),
-    Cycling(ActivityValue)
+struct ActivityRecord {
+    start: DateTime<FixedOffset>,
+    end: DateTime<FixedOffset>,
+    activity: ActivityKind,
+    distance: f64
 }
 
-
-impl Activity {
-    fn try_from_placemark(elem: Placemark) -> Option<Activity> {
-        let ext_data = elem.children.iter()
+impl ActivityRecord {
+    fn try_from_placemark(placemark: Placemark) -> Option<ActivityRecord> {
+        let ext_data = placemark.children.iter()
             .find(|e| e.name == "ExtendedData")?;
         let category_value = ext_data.children.iter()
             .find(|e| e.attrs.get("name") == Some(&"Category".to_string()))?
@@ -48,33 +118,41 @@ impl Activity {
             .content?
             .parse::<f64>()
             .expect("Couldn't parse distance");
-        let timespan = elem.children.iter()
+        let timespan = placemark.children.iter()
             .find(|e| e.name == "TimeSpan")?;
         let start_str = timespan.children.iter()
             .find(|e| e.name == "begin")?
             .content.clone()?;
+        let end_str = timespan.children.iter()
+            .find(|e| e.name == "end")?
+            .content.clone()?;
+
         let start = DateTime::parse_from_rfc3339(&start_str)
             .expect("Couldn't parse begin time");
+        let end = DateTime::parse_from_rfc3339(&end_str)
+            .expect("Couldn't parse end time");
         match category_value.as_str() {
-            "Driving" => Some(Activity::Driving(ActivityValue { distance: distance_value, time: start })),
-            "Cycling" => Some(Activity::Cycling(ActivityValue { distance: distance_value, time: start })),
+            "Driving" => Some(ActivityRecord {
+                start: start,
+                end: end,
+                activity: ActivityKind::Driving,
+                distance: distance_value }),
+            "Cycling" => Some(ActivityRecord {
+                start: start,
+                end: end,
+                activity: ActivityKind::Cycling,
+                distance: distance_value}),
             _ => None
         }
     }
 }
 
-#[derive(Debug)]
-struct ActivityValue {
-    distance: f64,
-    time: DateTime<FixedOffset>
-}
-
-fn unpack(kml: Kml<f64>) -> Vec<Activity> {
+fn unpack(kml: Kml<f64>) -> Vec<ActivityRecord> {
     match kml {
         Kml::KmlDocument(kd) => kd.elements.into_iter().map(|e| unpack(e)).flatten().collect(),
         Kml::Document { attrs: _a, elements: e} => e.into_iter().map(|x| unpack(x)).flatten().collect(),
         Kml::Placemark(p) => {
-            match Activity::try_from_placemark(p) {
+            match ActivityRecord::try_from_placemark(p) {
                 Some(a) => vec![a],
                 None => vec![]
             }
